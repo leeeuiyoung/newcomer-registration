47eaafa8f940041e785121c82d3d0105618198a5
leeeuiyoung <leeeuiyoung@naver.com>
2025-05-28 01:05:53 +0900

    Update tailwind.config.cjs

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,6 +1,5 @@
-
-/** @type {import('tailwindcss').Config} */
-module.exports = {}
+// tailwind.config.cjs
+module.exports = {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
@@ -9,4 +8,4 @@ module.exports = {}
     extend: {},
   },
   plugins: [],
-}
+};
